8e4e7162f58279d5d069b45649716e331089810e
dong5397git config --global user.email 깃ídongwok5397@naver.com <dongwok5397@naver.com>
2024-11-16 01:14:40 +0900

    디자인 및 더미데이터 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import EditorPage from './components/EditorPage.js'
 import SidebarPage from './components/SidebarPage.js'
+import EditorPage from './components/EditorPage.js'
 
 export default function App({ $target }) {
+    const dummyData = [
+        {
+            id: 1,
+            title: '제목1',
+            documents: [
+                {
+                    id: 2,
+                    title: '제목2',
+                    documents: [
+                        {
+                            id: 3,
+                            title: '제목3',
+                            documents: [],
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: 4,
+            title: '제목4',
+            documents: [],
+        },
+    ]
     const $listContainer = document.createElement('div')
     const $editorContainer = document.createElement('div')
 
-    $target.appendChild($listContainer)
     $target.appendChild($editorContainer)
+    $target.appendChild($listContainer)
 
-    const sidebarpage = new SidebarPage({ $target: $editorContainer })
+    const sidebarpage = new SidebarPage({
+        $target: $editorContainer,
+        initalState: dummyData,
+    })
     const editorpage = new EditorPage({ $target: $listContainer })
 }
